a4d7a43296ffd4f2c54716cc6c85fdff191c5cd2
Mario <mcc0115@alu.medac.es>
2025-05-09 19:00:25 +0200

    Combate y correcciones.
    Pequeños cambios y añadidos en el código y estructura de función de "combate()"  realizada, no está comprobado su funcionamiento, sin comentar.

--- funciones_main.py
+++ funciones_main.py
@@ -1,3 +1,4 @@
+import random
 from clase_inventario import clase_inventario
 from clase_jugador import clase_jugador 
 from clase_lista_nombres import lista_nombre
@@ -26,11 +27,47 @@ def combate():
     combate = int(input("¿Que quieres hacer? "))
 
     if combate == 1:
-        # Combate
-        print("a")
+        for i in range(int(jugador.cantidad_digipymon)):
+            contadorJug = 0
+            contadorEn = 0
+            tipos = ["Agua", "Fuego", "Planta"]
+            enemigo1.añadir_digipymon(digiEn = Digipymon((random.choice(lista_de_nombres.lista_nombres_digipymon), random.randint(10, 20), random.randint(0, 9), random.randint(0, 3), random.choice(tipos))))
+        
+        for x in range(int(jugador.cantidad_digipymon)):
+            digi_jugador = jugador.lista_digipymon[x]
+            digi_enemigo = enemigo1.lista_digipymon[x]
+
+            if digi_jugador.salud == 0:
+                print("Tu digipymon: " + digi_jugador.nombre + " no tiene salud. ¡Pierdes el combate!")
+                contadorEn += 1
+            elif digi_jugador.ataque > digi_enemigo.ataque:
+                print("El digipymon del jugador: " + digi_jugador.nombre + ", supera al digipymon enemigo: " + digi_enemigo.nombre)
+                diferencia = digi_jugador.ataque - digi_enemigo.ataque
+                digi_jugador.salud -= diferencia
+                contadorJug += 1
+                if digi_jugador.salud == 0:
+                    print("Tu digipymon ha caído en combate!")
+            elif digi_jugador.ataque < digi_enemigo.ataque:
+                print("El digipymon del jugador: " + digi_jugador.nombre + ", supera al digipymon enemigo: " + digi_enemigo.nombre)
+                diferencia = digi_jugador.ataque - digi_enemigo.ataque
+                digi_jugador.salud -= diferencia
+                contadorEn += 1
+                if digi_jugador.salud == 0:
+                    print("Tu digipymon ha caído en combate!")
+            elif digi_jugador.ataque == digi_enemigo.ataque:
+                print("Vuestros digipymon tienen el mismo ataque ¡Es una empate!")
+            
+        if contadorJug > contadorEn:
+            jugador.digicoins += contadorJug
+        elif contadorJug < contadorEn:
+            jugador.digicoins -= contadorEn 
+        
+        if jugador.digicoins <= 0:
+            jugador.digicoins = 0
+
     elif combate == 2:
         print("Has huido. Has perdido una digicoin")
-        jugador.digicoins = jugador.digicoins - 1
+        jugador.digicoins -= 1
 
         if jugador.digicoins <= 0:
             jugador.digicoins = 0
