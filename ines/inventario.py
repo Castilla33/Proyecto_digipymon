cda648c623e3987aeb0526fe191d3309c3ebc0b7
Ines <icg0024@alu.medac.es>
2025-05-17 00:19:05 +0200

    Comentar
    Clase inventario comentada

--- ines/inventario.py
+++ ines/inventario.py
@@ -1,13 +1,17 @@
+#Definimos la clase Inventario
 class Inventario:
+    #Método constructor y se ejecuta al crear un nuevo objeto de tipo Inventario
     def __init__(self):
-        self.objetos = {}
+        self.objetos = {} #Diccionario para guardar los objetos y sus cantidades
 
+    #Método para añadir objetos al inventario
     def añadir_objeto(self, nombre, cantidad):
         if nombre in self.objetos:
-            self.objetos[nombre] += 1
+            self.objetos[nombre] += 1 #Suma la cantidad en el caso en el que exista
         else:
-            self.objetos[nombre] = 1
+            self.objetos[nombre] = 1 #Crea esa cantidad en el caso de que no exista
 
+    #Método para usar un objeto del inventario
     def usar_objeto(self, objeto):
         self.objetos[objeto] -= 1
         if self.objetos [objeto] == 0:
